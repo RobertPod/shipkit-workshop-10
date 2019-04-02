638fe0a2ff8d04b1079823f0ed044d40d3bee470
Robert Podsiadly <rob.podsiadly@gmail.com>
2019-04-02 18:40:57 +0200

    Shipkit: Repeat.

--- src/main/java/com/example/shipkitworkshop/MathUtils.java
+++ src/main/java/com/example/shipkitworkshop/MathUtils.java
@@ -5,6 +5,6 @@ public class MathUtils {
     public static boolean isTriangle(int a, int b, int c) {
         return a + b > c
                 && a + c > b
-                && b + c > a;
+                && b + c >  a;
     }
 }
